f9229a667f933871e04d12a0ab36a10c1f2b86dc
codesche <codesche@gmail.com>
2025-10-06 21:52:10 +0900

    20251006 학습내용 정리 - 덩치

---
+++ src/bruteforce/baek7568/Baek7568_3.java
@@ -0,0 +1,66 @@
+package bruteforce.baek7568;
+
+import java.util.*;
+import java.io.*;
+
+// 3번째 반복
+public class Baek7568_3 {
+
+    static class Person {
+        int w;
+        int h;
+        int rank;
+
+        Person(int w, int h) {
+            this.w = w;
+            this.h = h;
+            this.rank = 1;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();             // 출력 고려
+
+        int N = Integer.parseInt(br.readLine());            // N 입력
+
+        // 객체 활용
+        List<Person> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int w = Integer.parseInt(st.nextToken());       // 몸무게
+            int h = Integer.parseInt(st.nextToken());       // 키
+            list.add(new Person(w, h));
+        }
+
+        // 모든 쌍을 비교: j가 i보다 (w, h) 둘 다 크면 i의 등수가 1 증가
+        for (int i = 0; i < N; i++) {
+            Person a = list.get(i);
+
+            for (int j = 0; j < N; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                Person b = list.get(j);
+
+                // 무게와 키가 다 클 경우에만 rank를 증가시킴
+                if (b.w > a.w && b.h > a.h) {
+                    a.rank++;
+                }
+            }
+        }
+
+        // 공백 출력
+        for (int i = 0; i < N; i++) {
+            sb.append(list.get(i).rank);
+
+            if (i < N - 1) {
+                sb.append(" ");
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
